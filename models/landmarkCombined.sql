6bf07d9acb591304a7836a560455819d0c7cf81d
ghavranek@inlytica.com <ghavranek@inlytica.com>
2021-10-19 11:21:39 +0000

    First Phase Landmark Done

--- models/landmarkCombined.sql
+++ models/landmarkCombined.sql
@@ -3,13 +3,39 @@ WITH landmarkGravityForm AS (
     WHERE form_name = 'liveatlandmarkwhitefish_form1'
 ),
 landmarkCrm AS (
-    SELECT * FROM combinedTables.crmCombined
-    WHERE CRM_System = 'landmarkHubspot'
-)
+    SELECT 
+        Email AS CRM_Email,
+        Contact_Created_Date AS CRM_Contact_Created_Date,
+        Last_Modified_Date AS CRM_Last_Modified_Date,
+        Lead_Source AS CRM_Lead_Source,
+        Contact_Type AS CRM_Contact_Type,
+        dealId AS CRM_dealId,
+        Deal_Name AS CRM_Deal_Name,
+        Stage_Original AS CRM_Stage_Original,
+        Deal_Description AS CRM_Deal_Description,
+        Amount AS CRM_Amount,
+        Deal_Created_Date AS CRM_Deal_Created_Date,
+        Deal_Probability AS CRM_Deal_Probability,
+        Deal_Closed_Date AS CRM_Deal_Closed_Date,
+        Deal_Closed_Reason AS CRM_Deal_Closed_Reason
 
-SELECT * FROM landmarkGravityForm 
+     FROM combinedTables.crmCombined
+    WHERE CRM_System = 'landmarkHubspot'
+),
+
+landmarkGoogleAnalytics AS (SELECT * FROM combinedTables.googleanalyticsCombined WHERE account = 'landmark'),
+
+gravityCRMCombined AS (SELECT * FROM landmarkGravityForm 
 LEFT JOIN landmarkCrm
-ON landmarkGravityForm.Email = landmarkCrm.Email
+ON landmarkGravityForm.Email = landmarkCrm.CRM_Email),
+
+gravityCRMGACombined AS (SELECT * FROM gravityCRMCombined LEFT JOIN landmarkGoogleAnalytics ON gravityCRMCombined.gaClientId = CAST(landmarkGoogleAnalytics.clientID AS FLOAT64))
+
+SELECT * EXCEPT(clientID, account) FROM gravityCRMGACombined
+
+
+
+
 
 
 
